e68e93a39519f8cab2a60363262d0577b9e395e0
Antont25 <toxafootbol1994@gmail.com>
2022-12-26 22:31:07 +0200

    feature: add products

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,60 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    const message = 'Hello  !';
+const port = 5000;
+let products = [{ id: 1, name: "tomato" }, { id: 2, name: "cucumber" }];
+app.use((0, body_parser_1.default)());
+app.get("/", (req, res) => {
+    const message = "Hello  !";
     res.send(message);
+});
+app.get("/products", (req, res) => {
+    if (req.query.title) {
+        let params = req.query.title.toString();
+        const productsFilter = products.filter(p => p.name.indexOf(params) > -1);
+        if (productsFilter.length > 0) {
+            res.send(productsFilter);
+            return;
+        }
+        res.send(404);
+    }
+    res.send(products);
+});
+app.get("/products/:productTitle", (req, res) => {
+    const product = products.find(p => p.name === req.params.productTitle);
+    if (product) {
+        res.send(product);
+        return;
+    }
+    res.send(404);
+});
+app.delete("/products/:id", (req, res) => {
+    if (req.params.id) {
+        const id = req.params.id.toString();
+        const newProducts = products.filter(p => p.id.toString() !== id);
+        if (newProducts.length < products.length) {
+            res.send(204);
+            products = newProducts;
+        }
+    }
+    res.send(404);
+});
+app.post("/products", (req, res) => {
+    const newProduct = { id: new Date().getMilliseconds(), name: req.body.title };
+    products.push(newProduct);
+    res.status(201).send(products);
+});
+app.put("/products/:id", (req, res) => {
+    const idProduct = req.params.id;
+    const updateProduct = products.find(p => p.id.toString() === idProduct);
+    if (updateProduct) {
+        updateProduct.name = req.body.title;
+        res.status(200).send(updateProduct);
+        return;
+    }
+    res.send(404);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
